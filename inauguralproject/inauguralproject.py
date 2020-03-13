2e0c92f3972d808a84884000f98e7f9971dd26c4
TobePo <htc444@alumni.ku.dk>
2020-03-13 09:04:56 +0100

    Progress

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -50,8 +50,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Question 1
-def u_func(c, l):
+def u_func(c, l,):
     return np.log(m + w * l - (t_0 * w * l + t_1 * max(w * l-kappa,0))) - v*l **(1+1/eps)/(1+1/eps)
+
 
 m = 1
 v = 10
@@ -62,16 +63,17 @@ kappa = 0.4
 #w = 0.5
 #c = np.log(m + w * l - (t_0 * w * l + t_1 * max(w * l-kappa,0)))
 
+
 #pre define lists
 l_plot = []
 c_plot = []
 w_plot = []
 
-for w in np.arange(0.5,1.5,0.05):
+for w in np.arange(0.5,1.5 + 0.05,0.05):
     def print_solution(c,l,u):
-        print(f'c = {c:.8f}')
-        print(f'l = {l:.8f}')
-        print(f'u  = {u:.8f}')
+        print(f'c = {c:.3f}')
+        print(f'l = {l:.3f}')
+        print(f'u  = {u:.3f}')
 
     def value_of_choice(l,m,v,eps,t_0,t_1,kappa,w):
         c = np.log(m + w * l - (t_0 * w * l + t_1 * max(w * l-kappa,0)))
@@ -87,8 +89,8 @@ for w in np.arange(0.5,1.5,0.05):
     print_solution(c,l,u)
 
     # Store results as lists
-    l_plot.append(f'{l:.8}')
-    c_plot.append(f'{c:.8}')
+    l_plot.append(f'{l:.3}')
+    c_plot.append(f'{c:.3}')
     w_plot.append(w)
 
 #print the newly stored lists (to see that they are stored correctly)
@@ -101,15 +103,11 @@ print(l_plot)
 print(c_plot)
 print(w_plot)
 
-plt.scatter(w_plot,l_plot)
+plt.plot(w_plot,l_plot)
 plt.show()
 
-plt.scatter(w_plot,c_plot)
+plt.plot(w_plot,c_plot)
 plt.show()
-
-
-w = range(0.5, 1.5, 0.1)
-plt.plot(u)
 
 #Question 3
 N = 10000
@@ -119,4 +117,49 @@ def tax_rev(l):
 print(tax_rev)
 
 #Question 4
-eps_ny = 0.1
+eps = 0.1
+
+#pre define lists
+l_plot_ny = []
+c_plot_ny = []
+w_plot_ny = []
+
+for w in np.arange(0.5,1.5 + 0.05,0.05):
+    def print_solution(c,l,u):
+        print(f'c = {c:.3f}')
+        print(f'l = {l:.3f}')
+        print(f'u  = {u:.3f}')
+
+    def value_of_choice(l,m,v,eps,t_0,t_1,kappa,w):
+        c = np.log(m + w * l - (t_0 * w * l + t_1 * max(w * l-kappa,0)))
+        return -u_func(c,l)
+
+    sol_case = optimize.minimize_scalar(
+        value_of_choice,method='bounded',
+        bounds=(0,1),args=(m,v,eps,t_0,t_1,kappa,w))
+
+    l = sol_case.x
+    c = np.log(m + w * l - (t_0 * w * l + t_1 * max(w * l-kappa,0)))
+    u = u_func(c,l)
+    print_solution(c,l,u)
+
+    # Store results as lists
+    l_plot.append(f'{l:.3}')
+    c_plot.append(f'{c:.3}')
+    w_plot.append(w)
+
+#print the newly stored lists (to see that they are stored correctly)
+print('l results: ' + str(l_plot_ny))
+print('c results: ' + str(c_plot_ny))
+print('w results: ' + str(w_plot_ny))
+
+#Question 2
+print(l_plot_ny)
+print(c_plot_ny)
+print(w_plot_ny)
+
+plt.plot(w_plot_ny,l_plot_ny)
+plt.show()
+
+plt.plot(w_plot_ny,c_plot_ny)
+plt.show()
