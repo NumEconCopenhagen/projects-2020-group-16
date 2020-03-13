2f1b32b2bf59db8902888845486cd90a285ecd6c
TobePo <htc444@alumni.ku.dk>
2020-03-13 09:38:14 +0100

    Nu tror jeg den er der

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,49 +1,3 @@
-def square(x):
-    """ square numpy array
-    
-    Args:
-    
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
-    
-    """
-    
-    y = x**2
-    return y
-
-"""
-import math
-import numpy as np
-from scipy import optimize
-
-# The function, budget constraint is in place of the consumption as the model is static, and utility is monotically increasing in c, so there is no reason not to specd everything
-def u_func(c, l, m = 1, eps = 0.3, v = 10, t_0 = 0.4, t_1 = 0.1, kappa = 0.4, w):
-    return math.log(m + w * l - (t_0 * w * l + t_1 * max(w * l - kappa, 0))) - v * l ** (1 + 1 / eps) / (1 + 1 / eps)
-
-# assigning values to the variables
-
-#m = 1
-#eps = 0.3
-#v = 10
-#t_0 = 0.4
-#t_1 = 0.1
-#kappa = 0.4
-
-# Plot l* and c* as functions of w
-import matplotlib.pyplot as plt
-N = 10000
-
-# Calculate the tax revenue
-def tax_rev:
-    return sum(t_0 * w * l + t_1 * max(w * l - kappa, 0))
-
-# New epsilon
-eps = 0.1
-"""
-
 import math
 from scipy import optimize
 import numpy as np
@@ -79,8 +33,8 @@ for w in np.arange(0.5,1.5 + 0.05,0.05):
         c = np.log(m + w * l - (t_0 * w * l + t_1 * max(w * l-kappa,0)))
         return -u_func(c,l)
 
-    sol_case_ny = optimize.minimize_scalar(
-        value_of_choice_ny,method='bounded',
+    sol_case = optimize.minimize_scalar(
+        value_of_choice,method='bounded',
         bounds=(0,1),args=(m,v,eps,t_0,t_1,kappa,w))
 
     l = sol_case.x
